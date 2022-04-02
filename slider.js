88c47db4c660c584ee720cfa1a2259a0bee8c4c3
muhib7353 <98839666+muhib7353@users.noreply.github.com>
2022-04-03 00:11:42 +0500

    filee name changed

--- slider.js
+++ slider.js
@@ -1,13 +1,13 @@
 let i=0;
 let image=[];
 let time=4000;
-image[0]='/images/image1.jpeg';
-image[1]='/images/image2.jpeg';
-image[2]='/images/image3.jpeg';
-image[3]='/images/image4.jpeg';
-image[4]='/images/image5.jpeg';
-image[5]='/images/image6.jpeg';
-image[6]='/images/image7.jpeg';
+image[0]='image1.jpeg';
+image[1]='image2.jpeg';
+image[2]='image3.jpeg';
+image[3]='image4.jpeg';
+image[4]='image5.jpeg';
+image[5]='image6.jpeg';
+image[6]='image7.jpeg';
 
 function changeImgage(){
      document.slide.src=image[i];
